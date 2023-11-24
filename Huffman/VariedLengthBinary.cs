aa8d647efdc9847bc8770cd523beda242f7c3a5e
Dominik Sebe <dominiksebe9@gmail.com>
2023-11-24 18:34:57 +0100

    Created VariedLengthBinary class.

---
+++ Huffman/VariedLengthBinary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman
+{
+    /// <summary>
+    /// Represents a binary value of variable bit-length.
+    /// </summary>
+    internal class VariedLengthBinary
+    {
+        #region Members
+        private Byte[] bytes;
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+        #endregion
+    }
+}
