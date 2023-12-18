62967c03db81d184ab8d7856b331b1d62111fc4e
Dominik Sebe <dominiksebe9@gmail.com>
2023-12-18 10:43:13 +0100

    Added documentation to HuffmanTree class.

--- Huffman/HuffmanTree.cs
+++ Huffman/HuffmanTree.cs
@@ -6,49 +6,83 @@ using System.Threading.Tasks;
 
 namespace Huffman
 {
+    /// <summary>
+    /// Represents implementation of a binary tree element, that stores a character and an integer value.
+    /// Implements Icomparable and stores an extra pointer to its parent.
+    /// </summary>
     internal class HuffmanTree : IComparable
     {
-        public delegate bool Search(HuffmanTree item);
-        public delegate void Process(HuffmanTree item);
-
+        #region Members
         private HuffmanTree parent, left, right;
         private char key;
         private int value;
+        #endregion
 
+        #region Properties
+        /// <summary>
+        /// Get the character value stored.
+        /// </summary>
         public char Key
         {
             get { return this.key; }
         }
+        /// <summary>
+        /// Get the integer value stored.
+        /// </summary>
         public int Value
         {
             get { return this.value; }
         }
+        /// <summary>
+        /// Get or set the pointer to the parent element.
+        /// </summary>
         public HuffmanTree Parent
         {
             get { return this.parent; }
             set { this.parent = value; }
         }
+        /// <summary>
+        /// Get or set the pointer to the element of the left brench.
+        /// </summary>
         public HuffmanTree Left
         {
             get { return this.left; }
             set { this.left = value; }
         }
+        /// <summary>
+        /// Get or set the pointer to the element of the right brench.
+        /// </summary>
         public HuffmanTree Right
         {
             get { return this.right; }
             set { this.right = value; }
         }
+        #endregion
 
+        #region Constructors
+        /// <summary>
+        /// Initializea new HuffmanTree object.
+        /// </summary>
+        /// <param name="key">Character value to store.</param>
+        /// <param name="value">Integer value to store.</param>
         public HuffmanTree(char key, int value)
         {
             this.key = key;
             this.value = value;
         }
+        #endregion
 
-        public static HuffmanTree Find(HuffmanTree root, Search func)
+        #region Methods
+        /// <summary>
+        /// Find an element that returns true for a passed function.
+        /// </summary>
+        /// <param name="root">The element to start the search from.</param>
+        /// <param name="func">The function to decide if the element is found or not.</param>
+        /// <returns>The element that returns true for the passed function or null if there is no such element.</returns>
+        public static HuffmanTree Find(HuffmanTree root, Func<HuffmanTree, bool> func)
         {
             HuffmanTree left = null, right = null;
-            if (root == null || func.Invoke(root)) return root;
+            if (root == null || func(root)) return root;
             else
             {
                 if (root.Left != null)
@@ -64,36 +98,60 @@ namespace Huffman
             return right;
 
         }
+        /// <summary>
+        /// Find an element by specifying which branch to search for each step.
+        /// </summary>
+        /// <param name="root">The element to start the search from.</param>
+        /// <param name="code">A VariedLengthBinary object to specify which branch to take at each step.</param>
+        /// <returns>The leaf element at the end of the steps.</returns>
         public static HuffmanTree FindCode(HuffmanTree root, VariedLengthBinary code)
         {
             if (root == null || root.Key != '\0') return root;
             else
             {
                 if (code[code.BitLength - 1])
-                    return FindCode(root.Right, code & ~(new VariedLengthBinary(1, 0) << (code.BitLength - 1)));
+                    return FindCode(root.Right, code & ~((VariedLengthBinary)1) << (code.BitLength - 1));
                 else
-                    return FindCode(root.Left, code & ~(new VariedLengthBinary(1, 0) << (code.BitLength - 1)));
+                    return FindCode(root.Left, code & ~((VariedLengthBinary)1) << (code.BitLength - 1));
             }
         }
-        public static void Map(HuffmanTree tree, Process func)
+        /// <summary>
+        /// Invoke a function for each element in the tree in Depth-first LNR order.
+        /// </summary>
+        /// <param name="tree">The element to start traversing from.</param>
+        /// <param name="func">The function to invoke for each elements</param>
+        public static void Traverse(HuffmanTree tree, Action<HuffmanTree> func)
         {
             if (tree != null)
             {
-                if (tree.Left != null) Map(tree.Left, func);
-                func.Invoke(tree);
-                if (tree.Right != null) Map(tree.Right, func);
+                if (tree.Left != null) Traverse(tree.Left, func);
+                func(tree);
+                if (tree.Right != null) Traverse(tree.Right, func);
             }
         }
+        #endregion
 
+        #region Functions
+        /// <summary>
+        /// Implementation of the CompareTo function. Greater is the HuffmanTree object, whose stored integer value is greater.
+        /// </summary>
+        /// <param name="obj">The HuffmanTree object to compare to.</param>
+        /// <exception cref="NullReferenceException">Thrown if the object can not be converted to a Huffmantree object.</exception>
+        /// <returns>Integer indicating if this or the other object is greater</returns>
         public int CompareTo(object obj)
         {
             if (this.value < (obj as HuffmanTree).Value) return -1;
             else if (this.value == (obj as HuffmanTree).Value) return 0;
             else return 1;
         }
+        /// <summary>
+        /// Return a string representing the HuffmanTree object.
+        /// </summary>
+        /// <returns>A string representing the HuffmanTree object.</returns>
         public override string ToString()
         {
             return String.Format("{{\"{0}\":\"{1}\"}}", this.key, this.value);
         }
+        #endregion
     }
 }
